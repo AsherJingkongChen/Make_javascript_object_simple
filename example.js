9f7e94c9e2b4f622ccbfe482949e074ee60bb734
ChenYiran <37398747+AsherJingkongChen@users.noreply.github.com>
2021-11-17 17:35:51 +0800

    Create example.js

---
+++ example.js
@@ -0,0 +1,35 @@
+function rec(obj, gen=0){
+  strip=""
+  for(i of [...Array(gen).keys()]){
+    strip+="  "
+  }
+  for(key in obj){
+    a=strip
+    b=key
+    idr=obj[key].constructor.name
+    idr=="Object" ? c="" : c=`- ${obj[key]}`
+    console.log(a,b,c)
+    if(c==""){
+      rec(obj[key],gen+1)
+      rec(obj[key],gen)
+    }
+  }
+}
+//below is my test
+objtest={
+  "aitem":{
+    "aaitem":{
+      "aaaitem":{
+        "twok":2000,
+        "array":[0,0,0]
+      },
+      "aaaitem2":"hai"
+    },
+    "aaitem2":"hi"
+  },
+  "bitem":{
+    "bbitem":"think"
+  },
+  "citem":17
+}
+rec(objtest)
